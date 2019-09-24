72f2238689282ef11e47e4f54350403ced10be32
Stefan Richter <stefan.richter@cern.ch>
2019-09-24 17:43:11 +0200

    [ISSUE #4] Add results of a quick feasibility study of l+l+l'- channel measurement
    The study is documented in the file high_purity_ttW_feasibility_study.md
    including its caveats and suggestions for follow-up.

---
+++ FeasibilityStudy/NtupleMaking/submit.sh
@@ -0,0 +1,112 @@
+prun \
+--inDS=mc16_13TeV.345940.aMcAtNloPythia8EvtGen_ttH_dilep.deriv.DAOD_TOPQ1.e6654_a875_r10724_p3629/ \
+--outDS=user.strichte.345940.TOPQ1.e6654_a875_r10724_p3629.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.345940.aMcAtNloPythia8EvtGen_ttH_dilep.deriv.DAOD_TOPQ1.e6654_a875_r10724_p3629_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.345940.aMcAtNloPythia8EvtGen_ttH_dilep.deriv.DAOD_TOPQ1.e6654_a875_r10724_p3629_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.364250.Sherpa_222_NNPDF30NNLO_llll.deriv.DAOD_TOPQ1.e5894_s3126_r10724_p3629/ \
+--outDS=user.strichte.364250.TOPQ1.e5894_s3126_r10724_p3629.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.364250.Sherpa_222_NNPDF30NNLO_llll.deriv.DAOD_TOPQ1.e5894_s3126_r10724_p3629_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.364250.Sherpa_222_NNPDF30NNLO_llll.deriv.DAOD_TOPQ1.e5894_s3126_r10724_p3629_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.364283.Sherpa_222_NNPDF30NNLO_lllljj_EW6.deriv.DAOD_TOPQ1.e6055_s3126_r10724_p3629/ \
+--outDS=user.strichte.364283.TOPQ1.e6055_s3126_r10724_p3629.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.364283.Sherpa_222_NNPDF30NNLO_lllljj_EW6.deriv.DAOD_TOPQ1.e6055_s3126_r10724_p3629_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.364283.Sherpa_222_NNPDF30NNLO_lllljj_EW6.deriv.DAOD_TOPQ1.e6055_s3126_r10724_p3629_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.410155.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttW.deriv.DAOD_TOPQ1.e5070_s3126_r10724_p3731/ \
+--outDS=user.strichte.410155.TOPQ1.e5070_s3126_r10724_p3731.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.410155.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttW.deriv.DAOD_TOPQ1.e5070_s3126_r10724_p3731_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.410155.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttW.deriv.DAOD_TOPQ1.e5070_s3126_r10724_p3731_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.410218.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee.deriv.DAOD_TOPQ1.e5070_a875_r10724_p3731/ \
+--outDS=user.strichte.410218.TOPQ1.e5070_a875_r10724_p3731.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.410218.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee.deriv.DAOD_TOPQ1.e5070_a875_r10724_p3731_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.410218.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee.deriv.DAOD_TOPQ1.e5070_a875_r10724_p3731_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.410219.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu.deriv.DAOD_TOPQ1.e5070_a875_r10724_p3731/ \
+--outDS=user.strichte.410219.TOPQ1.e5070_a875_r10724_p3731.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.410219.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu.deriv.DAOD_TOPQ1.e5070_a875_r10724_p3731_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.410219.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu.deriv.DAOD_TOPQ1.e5070_a875_r10724_p3731_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.410408.aMcAtNloPythia8EvtGen_tWZ_Ztoll_minDR1.deriv.DAOD_TOPQ1.e6423_s3126_r10724_p3654/ \
+--outDS=user.strichte.410408.TOPQ1.e6423_s3126_r10724_p3654.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.410408.aMcAtNloPythia8EvtGen_tWZ_Ztoll_minDR1.deriv.DAOD_TOPQ1.e6423_s3126_r10724_p3654_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.410408.aMcAtNloPythia8EvtGen_tWZ_Ztoll_minDR1.deriv.DAOD_TOPQ1.e6423_s3126_r10724_p3654_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
+prun \
+--inDS=mc16_13TeV.410560.MadGraphPythia8EvtGen_A14_tZ_4fl_tchan_noAllHad.deriv.DAOD_TOPQ1.e5803_s3126_r10724_p3629/ \
+--outDS=user.strichte.410560.TOPQ1.e5803_s3126_r10724_p3629.20190918_112054 \
+--useAthenaPackages \
+--cmtConfig=x86_64-slc6-gcc62-opt \
+--writeInputToTxt=IN:in.txt \
+--outputs=_root:mc16_13TeV.410560.MadGraphPythia8EvtGen_A14_tZ_4fl_tchan_noAllHad.deriv.DAOD_TOPQ1.e5803_s3126_r10724_p3629_20190918_112054-anatop.root \
+--exec="top-xaod mc16_13TeV.410560.MadGraphPythia8EvtGen_A14_tZ_4fl_tchan_noAllHad.deriv.DAOD_TOPQ1.e5803_s3126_r10724_p3629_20190918_112054-config.txt in.txt" \
+--maxNFilesPerJob=20 \
+--outTarBall=top-xaod.tar.gz \
+--mergeOutput \
+--memory=3000 \
+--destSE=DESY-HH_LOCALGROUPDISK \
+
